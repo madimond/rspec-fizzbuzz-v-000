11eb5a78752740dbdd0481822833aa61bd191e1a
Margaret Dimond <margaretdimond@gmail.com>
2018-05-17 02:03:42 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,11 @@
+
+
 def fizzbuzz(num)
   if num % 3 == 0
     puts "Fizz"
 elseif num % 5 == 0
   puts "Buzz"
-elseif (int % 3 == 0) && (int % 5 == 0)
+elseif (num % 3 == 0) && (num % 5 == 0)
   puts  "FizzBuzz"
 else nil
 end
